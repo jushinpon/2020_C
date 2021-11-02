4941dc4c376302e440f9dbcb8fbacd05cceeb3a0
jushinpon <32800219+jushinpon@users.noreply.github.com>
2021-11-02 08:59:23 +0800

    add plot.py

---
+++ plot.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
+savename = "test.jpg"
+data = np.genfromtxt("./test.txt", names=True)
+plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
+#plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
+
+plt.plot(data['Day'], data['improvement'], label='Improvement',marker='o',markevery= 20, linewidth=1.5, markersize=10)
+#plt.plot(data['Day'], data['test1'], label='test1',marker='v',markevery= 1, linewidth=1.5, markersize=10)
+plt.legend()
+plt.xlabel('Day')
+plt.ylabel('Improvement')
+#plt.savefig(savename)
+#plt.grid()
+plt.show()
